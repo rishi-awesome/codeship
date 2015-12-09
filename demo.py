b2cb067afcbd6f77d7bef9154166e9abcab37aeb
Sourabh Tiwari <sourabhkt@gmail.com>
2015-12-09 19:52:56 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 driver = webdriver.Chrome()
 driver.get("https://www.google.co.in/flights/#search;f=RPR;t=BOM;d=2015-12-21;r=2015-12-29;tt=o;sel=RPRBOM09W378")
+print driver.title
 if not "Flights to Mumbai - Google Flights" in driver.title:
     raise Exception("Unable to load google page!")
 elem = driver.find_element_by_css_selector("div.tooltipUseHtml div")

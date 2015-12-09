9fa7d90926ccaca8544b79087c3eaddd46d92ad9
Sourabh Tiwari <sourabhkt@gmail.com>
2015-12-09 19:46:30 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 driver = webdriver.Chrome()
 driver.get("http://www.google.com")
-print driver.title
 if not "Google" in driver.title:
     raise Exception("Unable to load google page!")
 elem = driver.find_element_by_name("q")

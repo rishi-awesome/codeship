af63c56a0a587e454fe991ab85f661c9b6490c9a
Sourabh Tiwari <sourabhkt@gmail.com>
2015-12-09 19:42:04 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,12 +3,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 driver = webdriver.Chrome()
-driver.get("http://www.google.com")
-if not "Google" in driver.title:
+driver.get("https://www.google.co.in/flights/#search;f=RPR;t=BOM;d=2015-12-21;r=2015-12-29;tt=o;sel=RPRBOM09W378")
+if not "Flights to Mumbai - Google Flights" in driver.title:
     raise Exception("Unable to load google page!")
-elem = driver.find_element_by_name("q")
-elem.send_keys("Coverfox")
-elem.submit()
-#print
-print driver.title
+elem = driver.find_element_by_css_selector("div.tooltipUseHtml div")
+fare = elem.text()
+print fare
 driver.quit()
